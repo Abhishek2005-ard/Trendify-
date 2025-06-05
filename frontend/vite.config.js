fdb48b5f45e8cfb603aa25a6847476720b5f9b3a
Akashy-cyber1 <akashy1935@gmail.com>
2025-06-05 20:39:32 +0530

    config change

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,20 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: { port: 5173 },
+// });
+
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
   plugins: [react()],
-  server: { port: 5173 },
-});
+  root: '.', // or './src' if your main files are in src
+  build: {
+    outDir: 'dist',
+  },
+})
